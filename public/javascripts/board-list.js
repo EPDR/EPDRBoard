cff932548032376c5ae1d890d4c4ebdfebaa8d9e
YOOGOMJA <machun3@naver.com>
2017-05-03 17:29:59 +0900

    DEV BRANCH FIRST

---
+++ public/javascripts/board-list.js
@@ -0,0 +1,5 @@
+angular.module('epdrboardApp').controller('listCtrl' , function($s){
+    console.log('hi')
+
+});
+
--- routes/board.js
+++ routes/board.js
@@ -5,12 +5,28 @@ var Board = require('../DB/Models/Board.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    Board.findAll().then(function(data , err){
-        res.render('board/list' , { title :'hello list page' , data : data });    
-    } , function(){
-        res.render('../error.jade');
-    })
+    //Board.findAll().then(function(data , err){
+    //    res.render('board/list' , { title :'hello list page' , data : data });    
+    //} , function(){
+    //    res.render('../error.jade');
+    //})
+    //
     
+    // GET PAGES
+    var currentPage = 0;
+    if(Object.keys(req.query).length > 0 && req.query['page']){
+        currentPage = parseInt(req.query['page']);
+
+    }
+
+    Board.findAndCountAll({
+        limit : 20,
+        offset : !isNaN(currentPage) ? (currentPage-1) *  20 : 1
+    }).then(function(result){
+        res.render('board/list' , {title : 'hello list page' , data : result.rows, count : result.count});
+    }, function(){
+        res.redirect('../');
+    });
 });
 
 
