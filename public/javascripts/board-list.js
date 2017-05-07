24511d1b11463901c100de903b6ef6b71bce2789
YOOGOMJA <machun3@naver.com>
2017-05-07 18:46:12 +0900

    글보기 삭제 완성

--- public/javascripts/board-list.js
+++ public/javascripts/board-list.js
@@ -185,39 +185,4 @@ window['_board'].mod
     }
 
     $s.fn.init().then($s.fn.pg.init);
-}]).filter('boardDT' , function(){
-    return function(obj){
-        var _tmp = new Date(obj);
-        return window['_board'].getTimeStamp(_tmp);
-    }
-});
-
-// angular외에 네이티브 스크립트 
-window['_board'].getTimeStamp = function(d){
-    // getTimeStamp
-    function ts(d) {
-        var s =
-            lz(d.getFullYear(), 4) + '-' +
-            lz(d.getMonth() + 1, 2) + '-' +
-            lz(d.getDate(), 2) + ' ' +
-
-            lz(d.getHours(), 2) + ':' +
-            lz(d.getMinutes(), 2) + ':' +
-            lz(d.getSeconds(), 2);
-
-        return s;
-    }
-    // loadingZeros
-    function lz(n, digits) {
-        var zero = '';
-        n = n.toString();
-
-        if (n.length < digits) {
-            for (i = 0; i < digits - n.length; i++)
-            zero += '0';
-        }
-        return zero + n;
-    }
-
-    return ts(d);
-}
+}])
--- public/javascripts/detail.js
+++ public/javascripts/detail.js
@@ -1,7 +1,40 @@
 window['_board'].mod
+.factory('detailFac' , ['$http' , function($http){
+    return {
+        get : function(){
+            return $http({
+                url : '/board/detail',
+                method : 'get',
+                params : {
+                    page : '0'
+                }
+            });
+        }
+    }
+}])
 .controller('detailCtrl' , ['$scope' , function($s){
     $s.UI = {
         load : false
+    };
+
+    $s.val = {
+        item : {}
     }
 
+    $s.fn = {
+        init : function(){
+            if(window.foo){
+                var _tmp = document.createElement('textarea');
+                _tmp.innerHTML = window.foo;
+                $s.val.item = JSON.parse(_tmp.innerText);
+            }
+            else{
+                // factory에서 HTTP실행
+            }
+            
+        }
+    }
+
+
+    $s.fn.init();
 }]);
